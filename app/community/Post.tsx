817c7d3c1994a38fa54ea6f1316beca415fedb35
Pawel Sobaniec <infipablosoban@gmail.com>
2022-12-27 14:52:26 +0100

    Add a like functionality

--- app/community/Post.tsx
+++ app/community/Post.tsx
@@ -1,5 +1,13 @@
 "use client";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  deleteDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  doc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { IPost } from "./Posts";
 import { UserAuth } from "../context/AuthContext";
@@ -10,7 +18,8 @@ interface Props {
 }
 
 interface Like {
-    userId: string;
+  userId: string;
+  likeId: string
 }
 
 export const Post = (props: Props) => {
@@ -23,15 +32,47 @@ export const Post = (props: Props) => {
 
   const getLikes = async () => {
     const data = await getDocs(likesDoc);
-    setLikesAmount(data.docs.length);
+    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })));
   };
 
+  const hasUserLiked = likes?.find((like) => like.userId === user.uid);
+
   async function addLike() {
-    await addDoc(likesRef, {
-      userId: user.uid,
-      postId: post.id,
-    });
+    try {
+      const newDoc = await addDoc(likesRef, {
+        userId: user.uid,
+        postId: post.id,
+      });
+      if (user) {
+        setLikes((prev) =>
+          prev ? [...prev, { userId: user.uid, likeId: newDoc.id }] : [{ userId: user?.uid, likeId: newDoc.id }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
+
+  async function removeLike() {
+    try {
+      const likeDelete = query(
+        likesRef,
+        where("postId", "==", post.id),
+        where("userId", "==", user.uid)
+      );
+
+      const likeData = await getDocs(likeDelete);
+      const likeId = likeData.docs[0].id;
+      const likeToDelete = doc(db, "likes", likeId);
+      await deleteDoc(likeToDelete);
+      if (user) {
+        setLikes((prev) => prev && prev.filter((like) => like.likeId != likeId));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="dark:bg-gray-700 bg-gray-50 shadow-md p-10 mb-3 mt-3 lg:w-1/3 rounded-lg">
       <div>
@@ -41,10 +82,13 @@ export const Post = (props: Props) => {
         <p className="text-sm text-gray-700 dark:text-gray-300">
           @{post.email}
         </p>
-        <button onClick={addLike} className="mt-3 bg-blue-500 p-1 rounded-lg">
-          👍
+        <button
+          onClick={hasUserLiked ? removeLike : addLike}
+          className="mt-3 bg-blue-500 p-1 rounded-lg"
+        >
+          {hasUserLiked ? "👎" : "👍"}
         </button>
-        {likesAmount && <p> Likes: {likesAmount} </p>}
+        {likes && hasUserLiked && <p> Likes: {likes?.length} </p>}
       </div>
     </div>
   );
